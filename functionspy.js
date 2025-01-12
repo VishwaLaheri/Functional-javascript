b2dcecafd4d6fec8b06f19efd4b8fbd6f4edebe4
Vishwa <laherivishwa01@gmail.com>
2025-01-12 23:01:02 +0530

    Update functionspy.js

--- functionspy.js
+++ functionspy.js
@@ -1,13 +1,10 @@
 function Spy(target, method) {
     const originalFunction = target[method];
     const spy = { count: 0 }; 
-  
     target[method] = function(...args) {
       spy.count++; 
       return originalFunction.apply(this, args); 
     };
-  
-    return spy; // Return the spy object
+    return spy; 
   }
-  
   module.exports = Spy;
